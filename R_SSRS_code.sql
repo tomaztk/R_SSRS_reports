a96dfb517d0fa2f416736a1e6e3f2edc1065595d
tomaztk <tomaz.kastrun@gmail.com>
2018-03-04 19:15:26 +0100

    Add files via upload

---
+++ R_SSRS_code.sql
@@ -0,0 +1,26 @@
+/*****************************************************
+Title: Using R in SQL Server Reporting Services (SSRS)
+Author: Tomaz Kastrun
+Blog: http://tomaztsql.wordpress.com
+Date: 03.03.2018
+*****************************************************/
+
+
+USE [master];
+GO
+
+
+CREATE DATABASE R_SSRS;
+GO
+
+USE R_SSRS;
+GO
+
+
+
+
+
+
+-- CLEAN UP
+DROP DATABASE IF EXISTS R_SSRS;
+GO
